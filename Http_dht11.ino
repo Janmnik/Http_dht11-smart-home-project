216fed3fffb9125eeb9493500078cc0527d77845
Janmnik <pyjamaman@janmnik.pl>
2022-05-27 17:31:19 +0200

    added README.md

--- Http_dht11.ino
+++ Http_dht11.ino
@@ -1,68 +1,82 @@
- #include "DHT.h"
+ #include <DHT.h>
  #include <ESP8266WiFi.h>
  #include <ESP8266HTTPClient.h>
 
- const char* ssid = "Network";
- const char* password = "password";
+
+ const char* ssid = "SSID";
+ const char* password = "PASS";
    
  DHT dht(2,DHT11);
+
+ unsigned  int Temperature=0;
+ unsigned  int Humidity=0;
+ unsigned long lastTime=0;
+ unsigned long timerDelay=5000;
  
- int Temperature=0;
- int Humidity=0;
  void setup()  
  { 
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED){
-    delay(1000);
+    delay(500);
     Serial.print("...");
     }
   Serial.println();  
-
  }  
  void loop()  
  {  
+  digitalWrite(LED_BUILTIN, HIGH); // turn the blue led off
+ if(( millis()-lastTime )> timerDelay){
   if (WiFi.status() == WL_CONNECTED){
-    HTTPClient http; //Declare an object of class HTTPClient
-    digitalWrite(LED_BUILTIN, HIGH); // turn the blue led off
+  WiFiClient client;  
+  HTTPClient http; 
+  
      float h = dht.readHumidity();
     // Read temperature as Celsius (the default)
     float t = dht.readTemperature();
     // Read temperature as Fahrenheit (isFahrenheit = true)
     float f = dht.readTemperature(true);
-  
     // Check if any reads failed and exit early (to try again).
     if (isnan(h) || isnan(t) || isnan(f)) {
-      //Serial.println(F("Failed to read from DHT sensor!"));
-      delay(500);
-      return;
+      Serial.println(F("Failed to read from DHT sensor!"));
+     delay(500);
     }else{
      //Serial.println("Temperature in C:");
-     if(t<0)
-       Temperature=(int)(dht.readTemperature( )*-1.897);
-     else
-       Temperature=(int)(dht.readTemperature( )*1.897);  
-       //Serial.println("Humidity in %:");  
-       Humidity=(int)(dht.readHumidity()-100);  
-       delay(1000);
+     if(t>0 || h<110)
+     {
+         Temperature=(int)(t);//*1.897);  
+         //Serial.println("Humidity in %:");  
+         Humidity=(int)(h);//-100);  
+     }else{http.end();}
       }
-      WiFiClient client;
-      String host="http://192.168.x.x/:5108/HomeCondition/Temperature/Add?temperatureC="+Temperature;
-      http.begin(client,host);
+      String TempString = String(Temperature);
+      String HumString = String(Humidity);
+      String host="http://192.168.0.20:5108/HomeCondition/Temperature/Add?temperatureC="+TempString;
+      if( t!=0 || h!=0 || h<100 ){
+      http.begin(client, host);
+      
       int httpCode = http.GET();
       if (httpCode > 0) { //Check the returning code
- 
       String payload = http.getString();  //Get the request response payload
-      Serial.println(payload);   //Print the response payload
- 
-    }
+      Serial.println("Code "+payload);   //Print the response payload
+      
+
+    }else{
+      Serial.println("Code "+(String)http.errorToString(httpCode).c_str());
+      }
+      }
       Serial.println("Temperature in C:");
       Serial.println(Temperature);
       Serial.println("Humidity in %:");
       Serial.println(Humidity);
-
       http.end();
-    }
-    delay(5000);
- }  
+     
+      
+  }else {
+      Serial.println("WiFi Disconnected");     
+      }
+      lastTime = millis();
+ }
+ }
+
---
+++ README.md
@@ -0,0 +1,29 @@
+## Http_dht11
+
+Biblioteki:
+
+Arduino_JSON
+by Arduino Wersja 0.1.0
+
+DHT sensor library
+by Adafruit Wersja 1.4.2
+
+Adafruit ESP8266
+by Adafruit Wersja 1.0.1
+
+Adres Api serwera IP: 192.168.0.20 /24 Api
+
+
+~Response Code~
+Error code:
+temp: 12
+Humidity: 167
+
+OFF:
+temp: 0
+Humidity: 0
+
+---------------------------------------------------------------
+The temperature data is currently being transferred in the program.
+The humidity data can be sent if changed variable name host.
+"IP:PORT&field1="+TempString&field2="+HumString
